d1cc3f8f437d1d98daab1715a9d16119a6a9826e
Robert Call <robertcall94@gmail.com>
2020-10-15 19:31:02 +0300

    Добавьте файлы проекта.

---
+++ Network_lab1/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Network_lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Server_Start_Button = new System.Windows.Forms.Button();
+            this.Server_Status_Label = new System.Windows.Forms.Label();
+            this.Send_Button = new System.Windows.Forms.Button();
+            this.Server_Stop_Button = new System.Windows.Forms.Button();
+            this.Server_progressBar = new System.Windows.Forms.ProgressBar();
+            this.time_label1 = new System.Windows.Forms.Label();
+            this.time_label2 = new System.Windows.Forms.Label();
+            this.IP_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Port_textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Server_Port_textBox = new System.Windows.Forms.TextBox();
+            this.Client_progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // Server_Start_Button
+            // 
+            this.Server_Start_Button.Location = new System.Drawing.Point(446, 12);
+            this.Server_Start_Button.Name = "Server_Start_Button";
+            this.Server_Start_Button.Size = new System.Drawing.Size(75, 23);
+            this.Server_Start_Button.TabIndex = 0;
+            this.Server_Start_Button.Text = "Start";
+            this.Server_Start_Button.UseVisualStyleBackColor = true;
+            this.Server_Start_Button.Click += new System.EventHandler(this.Server_Button_Click);
+            // 
+            // Server_Status_Label
+            // 
+            this.Server_Status_Label.AutoSize = true;
+            this.Server_Status_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Server_Status_Label.Location = new System.Drawing.Point(12, 92);
+            this.Server_Status_Label.Name = "Server_Status_Label";
+            this.Server_Status_Label.Size = new System.Drawing.Size(131, 20);
+            this.Server_Status_Label.TabIndex = 1;
+            this.Server_Status_Label.Text = "Server not listen";
+            // 
+            // Send_Button
+            // 
+            this.Send_Button.Location = new System.Drawing.Point(446, 282);
+            this.Send_Button.Name = "Send_Button";
+            this.Send_Button.Size = new System.Drawing.Size(75, 23);
+            this.Send_Button.TabIndex = 2;
+            this.Send_Button.Text = "Send";
+            this.Send_Button.UseVisualStyleBackColor = true;
+            this.Send_Button.Click += new System.EventHandler(this.Send_Button_Click);
+            // 
+            // Server_Stop_Button
+            // 
+            this.Server_Stop_Button.Location = new System.Drawing.Point(538, 12);
+            this.Server_Stop_Button.Name = "Server_Stop_Button";
+            this.Server_Stop_Button.Size = new System.Drawing.Size(75, 23);
+            this.Server_Stop_Button.TabIndex = 3;
+            this.Server_Stop_Button.Text = "Stop";
+            this.Server_Stop_Button.UseVisualStyleBackColor = true;
+            this.Server_Stop_Button.Click += new System.EventHandler(this.Stop_Button_Click);
+            // 
+            // Server_progressBar
+            // 
+            this.Server_progressBar.Location = new System.Drawing.Point(16, 133);
+            this.Server_progressBar.Name = "Server_progressBar";
+            this.Server_progressBar.Size = new System.Drawing.Size(758, 23);
+            this.Server_progressBar.TabIndex = 4;
+            // 
+            // time_label1
+            // 
+            this.time_label1.AutoSize = true;
+            this.time_label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.time_label1.Location = new System.Drawing.Point(263, 92);
+            this.time_label1.Name = "time_label1";
+            this.time_label1.Size = new System.Drawing.Size(96, 20);
+            this.time_label1.TabIndex = 5;
+            this.time_label1.Text = "Time spend";
+            // 
+            // time_label2
+            // 
+            this.time_label2.AutoSize = true;
+            this.time_label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.time_label2.Location = new System.Drawing.Point(505, 92);
+            this.time_label2.Name = "time_label2";
+            this.time_label2.Size = new System.Drawing.Size(74, 20);
+            this.time_label2.TabIndex = 6;
+            this.time_label2.Text = "Time left";
+            // 
+            // IP_textBox
+            // 
+            this.IP_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.IP_textBox.Location = new System.Drawing.Point(32, 282);
+            this.IP_textBox.Name = "IP_textBox";
+            this.IP_textBox.Size = new System.Drawing.Size(200, 26);
+            this.IP_textBox.TabIndex = 7;
+            this.IP_textBox.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(28, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "IP Adress";
+            // 
+            // Port_textBox
+            // 
+            this.Port_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Port_textBox.Location = new System.Drawing.Point(238, 282);
+            this.Port_textBox.Name = "Port_textBox";
+            this.Port_textBox.Size = new System.Drawing.Size(100, 26);
+            this.Port_textBox.TabIndex = 9;
+            this.Port_textBox.Text = "8888";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(234, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Port";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "c:\\Users\\rober\\OneDrive\\Рабочий стол\\Институт\\Nets\\";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.DesktopDirectory;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(39, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Port";
+            // 
+            // Server_Port_textBox
+            // 
+            this.Server_Port_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Server_Port_textBox.Location = new System.Drawing.Point(43, 30);
+            this.Server_Port_textBox.Name = "Server_Port_textBox";
+            this.Server_Port_textBox.Size = new System.Drawing.Size(100, 26);
+            this.Server_Port_textBox.TabIndex = 11;
+            this.Server_Port_textBox.Text = "8888";
+            // 
+            // Client_progressBar
+            // 
+            this.Client_progressBar.Location = new System.Drawing.Point(16, 337);
+            this.Client_progressBar.Name = "Client_progressBar";
+            this.Client_progressBar.Size = new System.Drawing.Size(758, 23);
+            this.Client_progressBar.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Client_progressBar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Server_Port_textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Port_textBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IP_textBox);
+            this.Controls.Add(this.time_label2);
+            this.Controls.Add(this.time_label1);
+            this.Controls.Add(this.Server_progressBar);
+            this.Controls.Add(this.Server_Stop_Button);
+            this.Controls.Add(this.Send_Button);
+            this.Controls.Add(this.Server_Status_Label);
+            this.Controls.Add(this.Server_Start_Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button Server_Start_Button;
+        public System.Windows.Forms.Label Server_Status_Label;
+        public System.Windows.Forms.Button Server_Stop_Button;
+        public System.Windows.Forms.ProgressBar Server_progressBar;
+        public System.Windows.Forms.Label time_label1;
+        public System.Windows.Forms.Label time_label2;
+
+        public System.Windows.Forms.Button Send_Button;
+        private System.Windows.Forms.TextBox IP_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Port_textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Server_Port_textBox;
+        public System.Windows.Forms.ProgressBar Client_progressBar;
+    }
+}
+
---
+++ Network_lab1/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Threading;
+using System.IO;
+
+namespace Network_lab1
+{
+    public partial class Form1 : Form
+    {
+        Server server1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Server_Button_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string path = folderBrowserDialog1.SelectedPath;
+            try
+            {
+                int port = Int32.Parse(Server_Port_textBox.Text);
+                server1 = new Server(port, Dispatcher.CurrentDispatcher, this, path);// @"c:\Users\rober\OneDrive\Рабочий стол\Институт\Nets\server");
+                Thread serverThread = new Thread(new ThreadStart(server1.Start));
+                serverThread.Start();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Unable to parse port");
+            }
+        }
+        private void Stop_Button_Click(object sender, EventArgs e)
+        {
+            if (server1 != null)
+            {
+                server1.work = false;
+                Server_Status_Label.Text = "Shutdown server ....";
+            }
+        }
+
+        private void Send_Button_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+            try
+            {
+                int port = Int32.Parse(Port_textBox.Text);
+                Client client1 = new Client(IP_textBox.Text, port, filename, Dispatcher.CurrentDispatcher, this);// @"c:\Users\rober\OneDrive\Рабочий стол\Институт\Nets\client\image.jpg");
+                Thread clientThread = new Thread(new ThreadStart(client1.connect));
+                clientThread.Start();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Unable to parse port");
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (server1 != null) server1.work = false;
+        }
+    }
+
+    public class Client
+    {
+        string server;
+        int port;
+        string path;
+        Dispatcher dispatcher;
+        Form1 form;
+        public Client(string _server, int _port, string _path, Dispatcher _disp, Form1 _form)
+        {
+            server = _server;
+            port = _port;
+            path = _path;
+            dispatcher = _disp;
+            form = _form;
+        }
+        public void connect()
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            bool retry = true;
+            using (FileStream fstream = File.OpenRead(path))
+            {
+                while (retry)
+                {
+                    try
+                    {
+                        TcpClient client = new TcpClient();
+                        client.Connect(server, port);
+                        NetworkStream stream = client.GetStream();
+
+                        dispatcher.BeginInvoke(new Action<int>((_progress) => form.Client_progressBar.Value = _progress), 0);
+
+                        Thread.Sleep(10000);
+                        byte[] length = BitConverter.GetBytes(fileInfo.Length);
+                        byte[] name = Encoding.UTF8.GetBytes(fileInfo.Name);
+                        byte[] name_length = BitConverter.GetBytes(name.Length);
+                        byte[] file_data = new byte[32768];
+                        stream.Write(length, 0, length.Length);
+                        stream.Write(name_length, 0, name_length.Length);
+                        stream.Write(name, 0, name.Length);
+
+                        long progress = 0;
+                        do
+                        {
+                            int bytes = fstream.Read(file_data, 0, file_data.Length);
+                            progress += bytes;
+                            stream.Write(file_data, 0, bytes);
+                            dispatcher.BeginInvoke(new Action<int>((_progress) => form.Client_progressBar.Value = _progress), (int)(100 * progress / fileInfo.Length));
+                        } while (progress < fileInfo.Length);
+
+                        retry = false;
+                        // закрываем поток
+                        stream.Close();
+                        // закрываем подключение
+                        client.Close();
+                    }
+                    catch (IOException e)
+                    {
+                        var result = MessageBox.Show(e.Message + " Повторить попытку отправки?", "IOException", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (result == DialogResult.No)
+                            retry = false;
+                    }
+                    catch (SocketException e)
+                    {
+                        var result = MessageBox.Show(e.Message + " Повторить попытку отправки?", "SocketException", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (result == DialogResult.No)
+                            retry = false;
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.Message, "Exception");
+                    }
+                }
+            }
+        }
+    }
+
+    public class Server
+    {
+        public bool work = true;
+        int port;
+        Dispatcher dispatcher;
+        Form1 form;
+        string path;
+        public Server(int _port, Dispatcher _disp, Form1 _form, string _path)
+        {
+            port = _port;
+            dispatcher = _disp;
+            form = _form;
+            path = _path;
+        }
+        ~Server()
+        {
+            work = false;
+        }
+        public void Start()
+        {
+            TcpListener server = null; 
+            try
+            {
+                server = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                dispatcher.BeginInvoke(new Action<string>((message) => form.Server_Status_Label.Text = message), "Server started on 127.0.0.1:" + port);
+                server.Start();
+                while(work)
+                {
+                    if (server.Pending())
+                    {
+                        TcpClient client = server.AcceptTcpClient();
+
+                        byte[] data = new byte[32768];
+                        StringBuilder response = new StringBuilder();
+                        NetworkStream stream = client.GetStream();
+
+                        stream.Read(data, 0, 8);
+                        long length = BitConverter.ToInt64(data, 0);
+                        stream.Read(data, 0, 4);
+                        int name_length = BitConverter.ToInt32(data, 0);
+                        int bytes = stream.Read(data, 0, name_length);
+                        string name = Encoding.UTF8.GetString(data, 0, bytes);
+
+                        using (FileStream fstream = new FileStream($"{path}\\" + name, FileMode.OpenOrCreate))
+                        {
+                            dispatcher.BeginInvoke(new Action<string>((message) => form.Server_Status_Label.Text = message), "Start downloading file " + name);
+                            dispatcher.BeginInvoke(new Action<int>((_progress) => form.Server_progressBar.Value = _progress), 0);
+                            DateTime start = DateTime.Now;
+                            long progress = 0;
+                            do
+                            {
+                                progress += stream.Read(data, 0, data.Length);
+                                fstream.Write(data, 0, data.Length);
+                                DateTime now = DateTime.Now;
+                                TimeSpan time = now - start;
+                                TimeSpan time_left = new TimeSpan(time.Ticks * (length - progress) / progress);
+                                dispatcher.BeginInvoke(new Action<string>((message) => form.time_label1.Text = message), "Time0 passed " + time.ToString(@"hh\:mm\:ss"));
+                                dispatcher.BeginInvoke(new Action<string>((message) => form.time_label2.Text = message), "Time0 left " + time_left.ToString(@"hh\:mm\:ss"));
+                                dispatcher.BeginInvoke(new Action<int>((_progress) => form.Server_progressBar.Value = _progress), (int)(100 * progress / length));
+                                //for (int i = 0; i < 10000; i++) ;
+                            }
+                            while (progress != length); // stream.DataAvailable); // пока данные есть в потоке*/
+                            dispatcher.BeginInvoke(new Action<string>((message) => form.Server_Status_Label.Text = message), "Finish downloading file " + name);
+                        }
+
+                        // закрываем поток
+                        stream.Close();
+                        // закрываем подключение
+                        client.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+            }
+            dispatcher.BeginInvoke(new Action<string>((message) => form.Server_Status_Label.Text = message), "Server not listen");
+        }
+    }
+}
